2697db47ff69afbe8085d6994727c1eaa2e98c7f
Suluh Arwani <suluharwani007@gmail.com>
2024-12-16 16:07:12 +0700

    update

--- app/Database/Migrations/2024-12-16-082130_AlokasiPi.php
+++ app/Database/Migrations/2024-12-16-082130_AlokasiPi.php
@@ -6,13 +6,51 @@ use CodeIgniter\Database\Migration;
 
 class AlokasiPi extends Migration
 {
-    public function up()
+   public function up()
     {
-        //
+        $this->forge->addField([
+            'id' => [
+                'type'           => 'INT',
+                'constraint'     => 11,
+                'unsigned'       => true,
+                'auto_increment' => true,
+            ],
+            'id_material' => [
+                'type'       => 'INT',
+                'constraint' => 11,
+                'unsigned'   => true,
+            ],
+            'id_pi' => [
+                'type'       => 'INT',
+                'constraint' => 11,
+                'unsigned'   => true,
+            ],
+            'quantity' => [
+                 'type' => 'FLOAT',
+                'constraint' => 10.2,
+                'null' => true,
+            ],
+            'status' => [
+                'type'       => 'INT',
+                'constraint' => 11,
+                'unsigned'   => true,
+            ],
+            'updated_at' => [
+                'type' => 'datetime',
+                'null' => true,
+            ],
+            'deleted_at' => [
+                'type' => 'datetime',
+                'null' => true,
+            ],
+            'created_at datetime default current_timestamp',
+        ]);
+        $this->forge->addPrimaryKey('id');
+        $this->forge->createTable('alokasi_pi');
     }
 
     public function down()
     {
-        //
+        $this->forge->dropTable('alokasi_pi');
     }
 }
