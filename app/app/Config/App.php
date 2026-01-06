676e4719d4864a7b737c00912b78ad504489d50e
Suluh Arwani <suluharwani007@gmail.com>
2026-01-06 08:48:13 +0700

    update

--- app/app/Config/App.php
+++ app/app/Config/App.php
@@ -16,7 +16,7 @@ class App extends BaseConfig
      *
      * E.g., http://example.com/
      */
-    public string $baseURL = BASE;
+    public string $baseURL = 'http://cnf-cis.ol/';
 
     /**
      * Allowed Hostnames in the Site URL other than the hostname in the baseURL.
